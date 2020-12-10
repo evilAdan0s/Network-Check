9696999c02a8ca2457a51b08facf94c7da06846e
evilAdan0s <eviladan0s@gmail.com>
2020-12-10 22:45:29 +0800

    1.0

---
+++ Network-Check/Network-Check/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Network_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            if (args.Length >= 2)
+            {
+                switch (args[0])
+                {
+                    case "-h":
+                        if (program.getHTTPResponse(args[1]) == true)
+                        {
+                            Console.WriteLine("[*] HTTP can be used !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("[!] HTTP has been disabled!");
+                        }
+                        break;
+                    case "-t":
+                        if (program.getTCPResponse(args[1], int.Parse(args[2])) == true)
+                        {
+                            Console.WriteLine("[*] TCP can be used !");
+                        }
+                        else
+                        {
+                            Console.WriteLine("[!] TCP has been disabled!");
+                        }
+                        break;
+                    case "-d":
+                        IPAddress[] ipAddresses = program.getDNSResponse(args[1]);
+                        if (ipAddresses != null)
+                        {
+                            foreach (var ip in ipAddresses)
+                            {
+                                Console.WriteLine("[*] DNS: {0}  {1}",args[1], ip);
+                            }
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("[!] ERROR: Please check argument");
+            }
+        }
+
+        private bool getHTTPResponse(string url)
+        {
+            string target = "http://" + url ;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(target);
+            request.Method = "HEAD";
+            request.Timeout = 8000;
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                
+                return true;
+            }
+            catch (WebException e)
+            {
+                return false;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private bool getTCPResponse(string ip, int port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork ,SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(ip, port);
+                socket.Close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                socket.Close();
+                return false;
+            }
+        }
+
+        private IPAddress[] getDNSResponse(string domainName)
+        {
+
+            try
+            {
+                IPAddress[] ip = Dns.GetHostAddresses(domainName);
+                return ip;
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("[!] ERROR: Please check argument");
+                return null;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Network-Check
 使用C#实现的出网探测工具，可以快速测试当前环境所支持的出网方式
+
+# 编译环境
+- MacOS 10.14
+- Rider 2020
+- .Net Framework 3.5
+
+# 使用方法
+```
+Network-Check.exe -h <DomainName>
+                  -t <IP> <Port>
+                  -d <DomainName>
+```
+- -h 探测HTTP出网，指定域名，例如：`Network-Check.exe -h www.baidu.com`
+- -t 探测TCP出网，指定IP和端口，例如：`Network-Check.exe -t 192.168.1.1 4444`
+- -d 探测DNS出网，指定域名，如Cobalt Strike的DNS beacon绑定域名，会返回DNS查询出的IP，可以对照是否为Cobalt Strike server地址，例如：`Network-Check.exe -d www.evil.com`
+
+# 演示
+环境：Windows 7 Pro x64
+
+HTTP：
+![20201210224119BOPKPa](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201210224119BOPKPa.png)
+
+TCP，目标机器使用nc监听：
+![20201210224443ydKSQQ](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201210224443ydKSQQ.png)
+
+DNS：
+![20201210224514DDI4RY](https://adan0s-1256533472.cos.ap-nanjing.myqcloud.com/uPic/20201210224514DDI4RY.png)
